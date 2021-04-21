ee6ff5fef3802e5cf862648ede436e6b7ff19158
SilviuDinu <silviu_dinu@yahoo.com>
2021-04-21 13:10:15 +0300

    work in progress

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ from torchvision import transforms
 import sys
 import numpy as np
 import json
-from flask import Response
+from flask import Response, request
+from flask_cors import CORS, cross_origin
+from net import Net
 
+FILE = 'model.pth'
 
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 
@@ -28,7 +32,7 @@ def get_symptoms():
         csv_reader = csv.reader(csv_file, delimiter=',')
         for idx, rows in enumerate(csv_reader):
             if idx == 0:
-                raw = rows
+                raw = rows[0:len(rows)-1]
     
     mapped = []
     
@@ -45,6 +49,38 @@ def get_symptoms():
 
 @app.route('/api/v2/predict', methods=['POST'])
 def predict():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    ids = request.get_json()['ids']
+    req_symtpoms = request.get_json()['symptoms']
+    print(req_symtpoms)
+
+    net = Net()
+    net.load_state_dict(torch.load(FILE))
+    net.eval()
+    input = []
+
+    curr = os.path.dirname(__file__)
+    path = os.path.join(curr, r'data/csv-data/Training.csv')
+
+    with open(path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for idx, rows in enumerate(csv_reader):
+            if idx == 0:
+                symptoms = rows[0:len(rows)-1]
+            
+    for idx, symptom in enumerate(symptoms):
+        for id in ids:
+            if id == idx:
+                input.append(1)
+            else:
+                input.append(0)
+
+    print(input)
+    print(len(input))
+            
+    # data = torch.from_numpy(np.array(ids, dtype='float32'))
+    # print(data)
+    # X, y = data
+    # output = net(X)
+    return Response(json.dumps({'success': 'ok'}), mimetype='application/json')
 
 app.run()
