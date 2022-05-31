cf5962e74719d436ed0c74481bf36c79ec2d0fd1
SilviuDinu <silviu_dinu@yahoo.com>
2022-05-31 14:23:33 +0300

    add /emotions endpoint

--- app.py
+++ app.py
@@ -94,4 +94,52 @@ def predict():
 
     return Response(json.dumps({'id': int(disease_id), 'disease': disease_value}), mimetype='application/json')
 
+@app.route('/api/v2/emotions', methods=['POST'])
+def predict():
+    ids = request.get_json()['ids']
+    req_symtpoms = request.get_json()['symptoms']
+
+    net = Net()
+    net.load_state_dict(torch.load(FILE))
+    net.eval()
+
+    curr = os.path.dirname(__file__)
+    path = os.path.join(curr, r'data/csv-data/Testing_despression_processed.csv')
+    diseases_path = os.path.join(curr, r'data/csv-data/diseases_disertatie.csv')
+
+    with open(path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for idx, rows in enumerate(csv_reader):
+            if idx == 0:
+                symptoms = rows[0:len(rows)-1]
+            
+    for id in ids:
+        symptoms[id] = 1
+        
+    for idx, symptom in enumerate(symptoms):
+        if type(symptom) == str and symptom != 1:
+            symptoms[idx] = 0
+    
+
+    # THIS IS TO GRAB FIRST ROW FROM TESTING.CSV AND DOUBLE CHECK THE MODEL
+    # with open(path, "r") as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=',')
+    #     for idx, rows in enumerate(csv_reader):
+    #         if idx == 1:
+    #             symptoms = rows[0:len(rows)-1]
+    
+    arr = np.array(symptoms, dtype=np.float32)
+    print(symptoms)
+    X = torch.from_numpy(arr)
+    output = net(X.view(1, 139))
+    disease_id = torch.argmax(output[0])
+
+    with open(diseases_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for idx, rows in enumerate(csv_reader):
+            if disease_id == torch.tensor(int(rows[0])):
+                disease_value = rows[1]
+
+    return Response(json.dumps({'id': int(disease_id), 'disease': disease_value}), mimetype='application/json')
+
 app.run()
