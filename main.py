72e40a7c042a89141b95c47eb6fb7f86fb06db2a
SilviuDinu <silviu_dinu@yahoo.com>
2022-05-31 14:58:01 +0300

    add metrici performanta

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import torch.nn as nn
 import os
 import torch.nn.functional as F
 from net import Net
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+from prettytable import PrettyTable
 from buildTrainData import *
 from buildTestData import *
 
 FILE = 'final_5007_521_0.944acc.pth'
+
+curr = os.path.dirname(__file__)
+path = os.path.join(curr, r'data/csv-data/Testing_despression_processed.csv')
+diseases_path = os.path.join(curr, r'data/csv-data/diseases_disertatie.csv')
 
 train = TrainingData()
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
@@ -50,7 +57,13 @@ except:
 
 correct = 0
 total = 0
+preds = []
+targets = []
 net.eval()
+
+correct_GERD = 0
+total_GERD = 0
+
 with torch.no_grad():
     for data in testset:
         X, y = data
@@ -59,11 +72,52 @@ with torch.no_grad():
             # print(torch.argmax(i), y[idx])
             if torch.argmax(i) == y[idx]:
                 correct += 1
+                if torch.argmax(i) == torch.tensor(0):
+                    correct_GERD += 1
+                    total_GERD += 1
             total += 1
+            preds.append(torch.argmax(i))
+            targets.append(y[idx].squeeze())
+
+print(len(preds), len(targets), correct_GERD, total_GERD)
+
+conf_matrix = confusion_matrix(preds, targets)
+
+t = PrettyTable(['Class', 'Name', 'TP', 'TN', 'FP', 'FN',
+                               'Sensitivity', 'Specificity', 'Precision', 'F1'])
+nb_classes = 42
+
+TP = np.diag(conf_matrix)
+for c in range(nb_classes):
+    idx = torch.ones(nb_classes).bool()
+    idx[c] = 0
+
+    # conf_matrix[idx[:, None], idx].sum() - conf_matrix[idx, c].sum()
+    TN = conf_matrix[idx.nonzero()[:, None], idx.nonzero()].sum()
+
+    FP = conf_matrix[idx, c].sum()
+
+    FN = conf_matrix[c, idx].sum()
+
+    sensitivity = (TP[c] / (TP[c]+FN))
+    specificity = (TN / (TN+FP))
+    precision = (TP[c] / (TP[c] + FP))
+    F1 = 2 * (precision * sensitivity) / (precision + sensitivity)
+
+    
+    with open(diseases_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for idx, rows in enumerate(csv_reader):
+            if c == torch.tensor(int(rows[0])):
+                disease_value = rows[1]
+
+
+    t.add_row([c, disease_value, float(TP[c]), float(TN), float(FP), float(FN), round(float
+        (sensitivity), 3), round(float(specificity), 3), round(float(precision), 3), round(float(F1), 3)])
 
 accuracy = round(correct/total, 3)
 print("Accuracy: ", accuracy)
-
+print(t)
 
 if accuracy >= 0.90 and not os.path.exists(FILE):
     torch.save(net.state_dict(), FILE)
